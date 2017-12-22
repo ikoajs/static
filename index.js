0e85a4ef0efdb5aa690122684002fceab8a108ad
Jiangtao <321jiangtao@gmail.com>
2017-12-22 22:28:47 +0800

    add static cache

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 const fs = require('mz/fs')
 const path = require('path')
-const LruCache = require('lru-cache')
 const mime = require('mime-types')
 const { DefinedError } = require('ikoa-utils')
+const LruCache = require('lru-cache')
 
 module.exports = function koaStatic({
   dir = process.cwd(),
   gzip = false,
-  maxAge = 0,
+  maxAge = 1200,
   memory = false,
-  videoSeparateSize = 1048576
+  videoSeparateSize = 3145728,
+  setHeader = noop
 }) {
-  let cache = LruCache({ max: 1000,maxAge: 3000 * 60 * 60 })
-  return async function (ctx,next) {
-    const files = await read(dir,cache)
+  const cache = LruCache({maxAge: maxAge * 1000, max: 500})
+  const noCacheTypes = ['text/html']
+  return async function(ctx, next) {
+    await read(dir, cache)
+
     const pathname = ctx.URL.pathname
-    const url = pathname.replace('/',path.sep)
+    const url = pathname.replace('/', path.sep)
     const findPath = `${dir}${url}`
     const stats = cache.get(findPath)
-    let stream,range,start,end,size,type
+    let stream, range, start, end, type
 
     if (stats) {
       const length = stats.size
@@ -29,21 +32,46 @@ module.exports = function koaStatic({
 
       // 支持range走range，不支持走下面
       if (range) {
-        range = range.replace(/bytes=/,'').split('-')
+        range = range.replace(/bytes=/, '').split('-')
         start = Number(range[0])
         end = range[1] ? Number(range[1]) : start + videoSeparateSize
-        end = Math.min(length - 1,end)
+        end = Math.min(length - 1, end)
 
-        stream = fs.createReadStream(findPath,{ start,end })
+        stream = fs.createReadStream(findPath, { start, end })
 
         ctx.status = 206
-        ctx.set('Content-Range',`bytes ${start}-${end}/${length}`)
-        ctx.set('Accept-Ranges','bytes')
+        ctx.set('Content-Range', `bytes ${start}-${end}/${length}`)
+        ctx.set('Accept-Ranges', 'bytes')
         ctx.length = end - start + 1
 
-        stream.on('error',err => ctx.onerror(err))
+        stream.on('error', err => ctx.onerror(err))
         ctx.body = stream
       } else {
+        if (noCacheTypes.includes(type)) {
+          ctx.res.setHeader('Cache-Control', 'no-cache')
+        } else {
+          // https://segmentfault.com/a/1190000006741200
+          // 如果已经请求过一次的话 且在缓存有效期内走缓存
+          const etag = ctx.header['if-none-match']
+          const generatedEtag = getEtag(stats)
+          const lastCache = etag == generatedEtag ? cache.get(generatedEtag) : null
+
+          if (lastCache) {
+            if (Date.now() - lastCache.cTime < lastCache.maxAge * 1000) {
+              ctx.status = 304
+              return
+            }
+          } else {
+            ctx.etag = generatedEtag
+            ctx.res.setHeader('Cache-Control', `max-age: ${maxAge}`)
+            cache.set(generatedEtag, { maxAge: maxAge, cTime: Date.now() })
+          }
+        }
+
+        if (typeof setHeader === 'function') {
+          setHeader.apply(ctx)
+        }
+
         stream = fs.createReadStream(findPath)
         ctx.length = length
         ctx.type = type
@@ -60,15 +88,24 @@ module.exports = function koaStatic({
   }
 }
 
+// 生成唯一的标识： 没有stats的情况使用内容md5加密
+function getEtag(stats) {
+  let mtime = stats.mtime.getTime().toString(16)
+  let size = stats.size.toString(16)
+  return `"${size}-${mtime}"`
+}
+
+function noop() {}
+
 // 返回所有的文件，不包含. 软链接
-async function read(file,cache) {
+async function read(file, cache) {
   const stats = await fs.stat(file)
 
   if (stats.isDirectory()) {
     const files = await fs.readdir(file)
-    await Promise.all(files.map(k => read(path.join(file,k),cache)))
+    await Promise.all(files.map(k => read(path.join(file, k), cache)))
   } else if (stats.isFile() && !path.basename(file).startsWith('.')) {
-    cache.set(file,{ size: stats.size })
+    cache.set(file, stats)
   }
   return cache
 }
